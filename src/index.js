7022ed3297276b3e9a6eb1239bd3480ad3cbafb3
Pere Grau <peregm98@gmail.com>
2020-11-09 12:29:25 +0100

    Added exports.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 export { default } from './components/Slider'
 export { useSlider, useZoom } from './hooks'
+
+export {
+  Overlay as StyledOverlay,
+  SlideOverlay as StyledSlideOverlay,
+  Slider as StyledSlider,
+} from './components/Slider/Slider.css';
+
+export SlideIndicator from './components/SlideIndicator';
+export Slide from './components/Slide';
+export Dots from './components/Dots';
